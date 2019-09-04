85a90f7cf327829e939164d85aa29d0689f95c52
bulletTrain54 <mvishnurana@gmail.com>
2019-09-04 09:52:21 +0800

    Lab-5 c-programs

--- Lab05/c-programming/Question3/question3.c
+++ Lab05/c-programming/Question3/question3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Student {
     char *name; 
@@ -8,7 +9,25 @@ struct Student {
 
 struct Student john; 
 
-int main(){
+void getStudent(struct Student *s) {
+
+    printf("Type the name of the student: \n");
+    s->name = malloc(100);   // assume name has less than 100 letters 
+    fgets(s->name, 100, stdin);
+
+    printf("Type the student number: \n");
+    scanf("%d", &(s->id));
+
+    printf("Type the student enrollment option (D or X): \n");
+    scanf("%c", &(s->enroll)); // watch out for a bug here!
+
+    free(s->name); 
+    //free(s->enroll); 
+    return; 
+}
+
+int main() {
     printf("Hello, I am awesome!\n"); 
+    getStudent(&john); 
     return 0; 
 }
