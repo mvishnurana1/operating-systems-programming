38e0f32d0cd402f4779ae88e9f6e184bc57622d8
bulletTrain54 <mvishnurana@gmail.com>
2019-09-04 11:10:49 +0800

    unix programs update

--- Lab05/Unix-programming/Question1/unix-question1.c
+++ Lab05/Unix-programming/Question1/unix-question1.c
@@ -4,14 +4,13 @@
 int main(){
    pid_t pid; 
     
-   printf("Just one process so far, call fork...\n");
    pid = fork();
 
    if(pid == 0){
-        printf("I am a child process. My pid is %d\n", pid);
+        printf("child process pid = %d\n", pid);
    }  
    else {
-        printf("I am a parent process. My pid is %d\n", pid); 
+        printf("parent process pid = %d\n", pid); 
    }
 
    return 0;  
---
+++ Lab05/Unix-programming/Question2/unix-question2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+    pid_t pid; 
+
+    pid = fork(); 
+
+    if(pid == 0){
+        printf("I am a child process %d \n", getpid()); 
+    } else {
+        printf("I am a parent process %d \n", getpid());
+    }
+
+    return 0;  
+}
