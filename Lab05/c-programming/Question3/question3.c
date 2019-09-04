15f9d59d878b7024040cf999de54f7052abd0b64
bulletTrain54 <mvishnurana@gmail.com>
2019-09-04 09:09:38 +0800

    Lab-5 added

---
+++ Lab05/c-programming/Question3/question3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Student {
+    char *name; 
+    int id; 
+    char enroll; 
+}; 
+
+struct Student john; 
+
+int main(){
+    printf("Hello, I am awesome!\n"); 
+    return 0; 
+}
