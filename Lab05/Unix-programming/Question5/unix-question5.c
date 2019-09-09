83c60afad8fcf7c54b2d19657719c79d183cf71c
bulletTrain54 <mvishnurana@gmail.com>
2019-09-10 07:42:30 +0800

    week-5

--- Lab05/Unix-programming/Question5/unix-question5.c
+++ Lab05/Unix-programming/Question5/unix-question5.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-int main() {
-    pid_t pid; 
-    pid = fork(); 
-
-    if(pid == 0) {
-        execl("/bin/sh", "bin/sh", "-c", "./nopath", "NULL"); 
-    }
-
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
+  
+int main(void) 
+{ 
+    pid_t pid = fork(); 
+      
+    if ( pid == 0 ) 
+    { 
+       execl("/bin/sh", "bin/sh", "-c", "./nopath", "NULL"); 
+    } 
+  
     int status; 
-
+      
     waitpid(pid, &status, 0); 
-
-    if(WIFEXITED(status)){
-        int exit_status = WEXITSTATUS(status); 
-        printf("Exit status of the child was %d\n", exit_status); 
-    }
-
+  
+    if ( WIFEXITED(status) ) 
+    { 
+        int exit_status = WEXITSTATUS(status);         
+        printf("Exit status of the child was %d\n",  
+                                     exit_status); 
+    } 
     return 0; 
-}
+} 
